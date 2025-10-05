f347bbec1d4c7a28ce220f424eaac9ec10b42134
Javokhir Isaev <javokhirisaev@usf.edu>
2025-10-05 18:36:33 -0400

    step6 done: created company instance, added employees and managers, and tested output

--- cc_6.js
+++ cc_6.js
@@ -41,3 +41,13 @@ class Company {
     this.employees.forEach(emp => console.log(emp.describe()));
   }
 }
+
+//Step6
+const myCompany = new Company();
+
+myCompany.addEmployee(employee1);
+myCompany.addEmployee(employee2);
+myCompany.addEmployee(manager1);
+myCompany.addEmployee(manager2);
+
+myCompany.listEmployees();
