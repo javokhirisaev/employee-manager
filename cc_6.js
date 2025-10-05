1c3da5948b054b2a70494c612257591b4e659651
Javokhir Isaev <javokhirisaev@usf.edu>
2025-10-05 17:19:10 -0400

    step3 done: added Manager class with inheritance

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,15 @@ class Employee {
     return `${this.name} works in the ${this.department} department.`;
   }
 }
+
+//Step3
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department); 
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${this.name} manages the ${this.department} department with a team of ${this.teamSize} people.`;
+  }
+}
